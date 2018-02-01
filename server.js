e2db11202fea63a68fbd26b6fe323701be96cf84
David Benton <soccershelbyb15@gmail.com>
2018-01-31 22:07:14 -0500

    comments

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // Dev Notice! server is template from 14-handlebars acvivity 04
 
 // dependencies
-
 var express = require("express");
 var exphbs = require("express-handlebars");
+var parser = require("body-parser");
+var cheerio = require("cheerio")
+var mongoose = require("mongoose");
+var request = require("request");
 
 // creating express app
 var app = express();
@@ -15,8 +18,12 @@ var port = process.env.PORT || 3000;
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-// Data (will need to replace data with 
-// info scraped from cheerio and inserted into mongo)
+///////////////////////////////////////////////
+// Data will need to replace data with 
+// info scraped from cheerio and inserted into mongo
+///////////////////////////////////////////////
+
+// data (CHANGE "Lunch" titles)
 var lunches = [
     {
       headline: "Terroist attack",
@@ -28,17 +35,22 @@ var lunches = [
         url: "www.NYTimes.com"
     }
   ];
-  
-  // route using loop (see activity 11 for example of multiple iterations)
+  ///////////////////////////////////////////////
+  // route using loop (see handlebars activity 11 
+  // for example of multiple iterations)
+  ///////////////////////////////////////////////
+
+  // routes
   app.get("/", function(req, res) {
     for (i = 0; i < lunches.length; i++) {
         res.render("index", lunches[i]);
     }
   });
+
   // original route
-//  app.get("/", function(req, res) {
-//     res.render("index", lunches[0]);
-//   });
+    //  app.get("/", function(req, res) {
+    //     res.render("index", lunches[0]);
+    //   });
   
   
   // Initiate the listener.
